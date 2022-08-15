50fa8667f77b4dbf0fff325463dbe10d1b48a12d
황준현 <ihrug000@gmail.com>
2022-08-15 16:49:29 +0900

    react build error - window is undefined

--- src/components/theme-switch/index.js
+++ src/components/theme-switch/index.js
@@ -8,31 +8,70 @@ import './style.scss';
 
 function ThemeSwitch() {
   const [isDarkMode, setIsDarkMode] = useState(getValueFromLocalStorage('isDarkMode'));
+  const [ScrollY, setScrollY] = useState(0);
+  const [BtnStatus, setBtnStatus] = useState(false); // 버튼 상태
 
   useEffect(() => {
     setValueToLocalStorage('isDarkMode', isDarkMode);
     document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
   }, [isDarkMode]);
 
-  const handleScroll = (e) => {
-    if (!window.scrollY) {
-      return;
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+    if (ScrollY > 100) {
+      // 100 이상이면 버튼이 보이게
+      setBtnStatus(true);
+    } else {
+      // 100 이하면 버튼이 사라지게
+      setBtnStatus(false);
     }
-    // 현재 위치가 이미 최상단일 경우 return
+  };
 
+  const handleTop = () => {
+    // 클릭하면 스크롤이 위로 올라가는 함수
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
+    setScrollY(0); // ScrollY 의 값을 초기화
+    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
   };
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow);
+    };
+    watch();
+    return () => {
+      window.removeEventListener('scroll', handleFollow);
+    };
+  });
+
+  // const handleScroll = (e) => {
+  //   if (!window.scrollY) {
+  //     return;
+  //   }
+  //   // 현재 위치가 이미 최상단일 경우 return
+
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth',
+  //   });
+  // };
   return (
     <div>
       <div className="scroll-up-button-wrapper">
-        <IconButton className="scroll-up-button" onClick={handleScroll}>
+        <IconButton className="scroll-up-button" onClick={handleTop}>
           {isDarkMode ? (
-            <KeyboardDoubleArrowUpIcon className="dark-scroll-up-icon" fontSize="large" />
+            <KeyboardDoubleArrowUpIcon
+              className={BtnStatus ? 'dark-scroll-up-icon' : 'dark-scroll-up-icon-hidden'}
+              fontSize="large"
+            />
           ) : (
-            <KeyboardDoubleArrowUpIcon className="light-scroll-up-icon" fontSize="large" />
+            <KeyboardDoubleArrowUpIcon
+              className={BtnStatus ? 'light-scroll-up-icon' : 'light-scroll-up-icon-hidden'}
+              fontSize="large"
+            />
           )}
         </IconButton>
       </div>
